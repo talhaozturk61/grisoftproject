d90fc74a375672b6a2e1837de92fcad6e9146572
Talha Öztürk <talha.ozturk@cmosrealitylab.com>
2022-06-06 14:52:33 +0300

    views

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:urlshortener/startPage.dart';
+import 'package:urlshortener/views/startPage.dart';
 
 
 void main() {
